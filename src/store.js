92ec6f9b2b005a483e78a5b624ada4398fb6aa30
victor hugo <victor03016@gmail.com>
2019-08-13 22:29:07 +0200

    Agregar localStora en Stote

--- src/store.js
+++ src/store.js
@@ -1,14 +1,24 @@
 import { createStore } from "redux";
-
 import reducer from "./reducers/index";
+import {obtenerStateStorage, guardarStateStorage} from './localStorage'
 
 // definir el state inicial
-const initialState = [];
+// const initialState = [];
+
+// Obtener citas del localStorage
+const storageState = obtenerStateStorage();
 
 const store = createStore(
     reducer,
-    initialState,
+    //initialState,
+    storageState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe( () => {
+    guardarStateStorage({
+        citas : store.getState().citas
+    })
+} )
 
 export default store;
